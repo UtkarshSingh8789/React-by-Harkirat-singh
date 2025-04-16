6bf7c686b319e686bdd950d2d387b5f3658c4bb5
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-04-16 11:31:25 +0530

    concept of layout in react

--- class3React/hooks/src/App.jsx
+++ class3React/hooks/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter,Routes,Route, Link,useNavigate} from "react-router-dom"
+import {BrowserRouter,Routes,Route, Link,useNavigate, Outlet} from "react-router-dom"
 // there is another type of router HashRouter which is used in creating chrome extension
 // there are two reasons for which we use navigation first one that user wants to go to anothe page by clicking which is done by using link;
 // and the other reason ->suppose user go to another page 1dter 0 second but the requirement is that user have to go to home pag after 10 second
@@ -20,21 +20,48 @@ function App() {
       {/* link is always used inside browserRouter*/}
       <BrowserRouter>
       {/* we will route all our application in browserRoute */}
-      <Link to="/">Allen</Link>
-      <Link to="/neet/online-coaching-class-11"> Class 11</Link>
-      <Link to="/neet/online-coaching-class-12">Class 12</Link>
       <Routes>
         {/* conditional rendering */}
-        <Route path="/neet/online-coaching-class-11" element={<Class11Program/>}/>
-        {/* meaning of above line for this route given render the class11Program component */}
-        <Route path="/neet/online-coaching-class-12" element={<Class12Program/>}/>
-        <Route path="/" element={<Landing/>}/>
-        <Route path="*" element={<ErrorPage/>}/>
-        {/* this route when we go on random route which is not there then it redicts to this * route which renders error page */}
+        <Route path="/" element={<Layout/>}>
+        {/* meaning of above line anything which starts with "/" ushko is layout me wrap kr dena*/}
+          <Route path="/neet/online-coaching-class-11" element={<Class11Program/>}/>
+          {/* meaning of above line for this route given render the class11Program component */}
+          <Route path="/neet/online-coaching-class-12" element={<Class12Program/>}/>
+          <Route path="/" element={<Landing/>}/>
+          <Route path="*" element={<ErrorPage/>}/>
+          {/* this route when we go on random route which is not there then it redicts to this * route which renders error page */}
+        </Route>
       </Routes>
       </BrowserRouter>
     </div>
   )
+}
+// function Layout(){
+//   return <div style={{height:"100vh"}}>  
+//     <Link to="/">Allen</Link>
+//     <Link to="/neet/online-coaching-class-11"> Class 11</Link>
+//     <Link to="/neet/online-coaching-class-12">Class 12</Link>
+//     <div style={{height:"90vh"}}>
+//       <Outlet/>
+//     </div>
+//     footer
+//   </div>
+// } or
+function Layout(){
+  return <div style={{height:"100vh"}}>  
+    <Header/>
+    <div style={{height:"90vh"}}>
+      <Outlet/>
+    </div>
+    footer
+  </div>
+}
+function Header(){
+  return <div>
+    <Link to="/">Allen</Link>
+    <Link to="/neet/online-coaching-class-11"> Class 11</Link>
+    <Link to="/neet/online-coaching-class-12">Class 12</Link>
+  </div>
 }
 function Class11Program(){
   return <div>Neet Program for Class-11</div>
