61a2d963e382cf8a3c2f09d02b2f74cbc4239e3b
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-04-23 11:43:20 +0530

    memo

--- class4React/recoil/src/App.jsx
+++ class4React/recoil/src/App.jsx
@@ -57,59 +57,111 @@
 // export default App;
 
 
-import React from 'react'
-import {RecoilRoot, useRecoilValue, useSetRecoilState} from 'recoil'
-import {counterAtom}  from './store/atoms/counter.js'
+// import React from 'react'
+// import {RecoilRoot, useRecoilValue, useSetRecoilState} from 'recoil'
+// import {counterAtom}  from './store/atoms/counter.js'
+// function App(){
+//   return(
+//     // step 1- installing recoil;
+//     // step 2- wrapping our application in RecoilRoot;
+//     <RecoilRoot>
+//       <Counter/>
+//     </RecoilRoot>
+//   )
+// }
+// // const counter=atom({
+// //   key:"counter",// it is used to uniquely identify the atom
+// //   default:0
+// // })// creatung in different file 
+// function Counter(){
+//   // const [count,setCount]=useState(0); // step 3- replacing this with an atom
+
+//   return (
+//     <div>
+//       <CurrentCount/>
+//       <Increase/>
+//       <Decrease/>
+//     </div>
+//   )
+// }
+// //ATOMS-Units of state that can be read from and wriiten to from any component.When an atom's state changes,all components
+// // that subscribe to that atom will re-render
+// function CurrentCount(){
+//   const count=useRecoilValue(counterAtom)//this component is now subscribed to the value of this atom
+//   console.log(count)
+//   return <h2>Count: {count}</h2>;
+// }
+// function Decrease(){
+//   const setCount=useSetRecoilState(counterAtom)// ishko value ka access nhi chaiye ishko bs setter chaiye
+//   return (
+//     <div>
+//       <button onClick={()=>{
+//         setCount(c=>c-1)
+//       }}>Decrease</button>
+//     </div>
+//   )
+// }
+// function Increase(){
+//   const setCount=useSetRecoilState(counterAtom)
+//   return (
+//     <div>
+//       <button onClick={()=>{
+//         setCount(c=>c+1)
+//       }}>Increase</button>
+//     </div>
+//   )
+// }
+// export default App
+
+
+
+// CONCEPT OF MEMO  ;
+
+import { useEffect, useState,memo } from "react";
 function App(){
-  return(
-    // step 1- installing recoil;
-    // step 2- wrapping our application in RecoilRoot;
-    <RecoilRoot>
-      <Counter/>
-    </RecoilRoot>
+  return (
+    <Counter/>
   )
 }
-// const counter=atom({
-//   key:"counter",// it is used to uniquely identify the atom
-//   default:0
-// })// creatung in different file 
 function Counter(){
-  // const [count,setCount]=useState(0); // step 3- replacing this with an atom
-
+  const [count,setCount]=useState(0)
+  useEffect(()=>{
+    setInterval(()=>{
+      setCount(c=>c+1)
+    },3000)
+  },[])
+  // in Counter component rerenders will happen whenever the count changes that is at every 3 sec;
+  // since we are not passing state variables to its child components the re-render will still happens in its child component(CurrentCount,Increase,Decrease);
+  // the react is not smart enought we have to till the unless the prop changes in the child components do not re-renders it
+  // to fix it we use memo
   return (
     <div>
-      <CurrentCount/>
+      {/* <CurrentCount/> */}
+      <MemoizedCurrentCount/>
+      {/* it will not rerender unless the prop changes or state changes of that component*/}
       <Increase/>
       <Decrease/>
     </div>
   )
 }
-//ATOMS-Units of state that can be read from and wriiten to from any component.When an atom's state changes,all components
-// that subscribe to that atom will re-render
+const MemoizedCurrentCount=memo(CurrentCount)
 function CurrentCount(){
-  const count=useRecoilValue(counterAtom)//this component is now subscribed to the value of this atom
-  console.log(count)
-  return <h2>Count: {count}</h2>;
+  return <div>1</div>
 }
-function Decrease(){
-  const setCount=useSetRecoilState(counterAtom)// ishko value ka access nhi chaiye ishko bs setter chaiye
+//Another syntax of memo
+const Increase=memo(function (){
   return (
     <div>
-      <button onClick={()=>{
-        setCount(c=>c-1)
-      }}>Decrease</button>
+      <button onClick={()=>{}}>Increase</button>
     </div>
   )
-}
-function Increase(){
-  const setCount=useSetRecoilState(counterAtom)
+})
+const Decrease=memo(function (){
   return (
     <div>
-      <button onClick={()=>{
-        setCount(c=>c+1)
-      }}>Increase</button>
+      <button onClick={()=>{}}>Increase</button>
     </div>
   )
-}
+})
 export default App
 
