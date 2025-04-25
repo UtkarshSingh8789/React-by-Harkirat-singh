215fe8fb5521de5c5df34da62d3fc42b5208066e
UtkarshSingh8789 <utkarshkumarsingh42@gmail.com>
2025-04-25 17:37:55 +0530

    selectors,atom,useRecoilState

--- class4React/RecoilAdvance/src/App.jsx
+++ class4React/RecoilAdvance/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil"
+import { jobsAtom, messagingAtom, networkAtom, notificationsAtom, totalNotificationSelector } from "./Store/atoms.js"
+import { useMemo } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RecoilRoot>
+      <MainApp />
+    </RecoilRoot>
   )
 }
+function MainApp(){
+  const networkNotificationCount=useRecoilValue(networkAtom)
+  const jobsAtomCount=useRecoilValue(jobsAtom)
+  const notificationsAtomCount=useRecoilValue(notificationsAtom)
+  // const [messagingAtomCount,setMessagingAtomCount]=useRecoilState(messagingAtom)
+  const messagingAtomCount=useRecoilValue(messagingAtom)
+  // useRecoilState also gives us two things like useState
+  // if we only need (setMessagingAtomCount) this thing, if we only need to update things and not actually want the value of it we use this thing
 
+  //  COMMENTS:-
+//      Hook         |      Use case
+// useRecoilValue    | Only read atom value
+// useSetRecoilState | Only write/update atom value
+// useRecoilState    | Read and write atom value (2-way)
+  // const totalNotificationCount=useMemo(()=>{
+  //   return networkNotificationCount+jobsAtomCount+notificationsAtomCount+messagingAtomCount
+  // },[networkNotificationCount,jobsAtomCount,notificationsAtomCount,messagingAtomCount])//without selector
+  //Here, useMemo ensures that this addition only re-runs if any of the 4 counts change.
+  // this also can be done by selectors 
+  const totalNotificationCount=useRecoilValue(totalNotificationSelector)// with selector
+  return (
+    <div>
+      <button>Home</button>
+
+      <button>My Network ({networkNotificationCount>=99 ? "99+" : networkNotificationCount})</button>
+      <button>Jobs ({jobsAtomCount>=99 ? "99+" : jobsAtomCount})</button>
+      <button>Messaging ({messagingAtomCount>=99 ? "99+" : messagingAtomCount})</button>
+      <button>Notifications ({notificationsAtomCount>=99 ? "99+" : notificationsAtomCount})</button>
+
+      {/* <button onClick={()=>{
+        setMessagingAtomCount(messagingAtomCount+1)
+      }}>Me</button>  */}
+      <button>Me ({totalNotificationCount})</button>
+    </div>
+  )
+}
 export default App
